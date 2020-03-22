1f0eaeafad3260412e3f4ba20b989ac999c31b46
Olivverde19270 <oliver.milian00@gmail.com>
2020-03-21 23:20:33 -0600

    Final commit

--- src/translator/Association.java
+++ src/translator/Association.java
@@ -1,40 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package translator;
 
 import java.util.Map;
-
 /**
- *
- * @author jose
- */
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructura de datos
+ * 
+ * General Description:
+ * A program which main purpose is to store a wide brand of English words
+ * with their Spanish meaning. Implementing Association, Node and BinaryTree 
+ * classes.
+ * 
+ * Class Description:
+ * Association class, store the association implementation, allows the 
+ * storage of key-value words. Being in this case English-Spanish translation
+ * 
+ * @author olivverde
+ * Retrieved from: Java Structure. "Data Structure in Java for the Principled 
+ * Programmer". Cap. 12... http://dept.cs.williams.edu/~bailey/JavaStructures/Book_files/JavaStructures.pdf
+ * */
 public class Association<K, V> implements Map.Entry<K,V> 
 {
+	//Attributes
 	protected K key;
 	protected V value;
 
+	//Constructor
 	public Association(K key, V value) 
         {
             this.key = key;
             this.value = value;
 	}
-
+	/**
+	 * Gets the key
+	 */
     @Override
     public K getKey() 
     {
         return key;
         
     }
-
+    /**
+     * Gets the value
+     */
     @Override
     public V getValue() 
     {
         return value;
     }
-
+    /**
+     * Sets a value
+     */
     @Override
     public V setValue(V v) 
     {
