c578d3f1869240fdd8944159b1ede800a4304d21
Olivverde19270 <oliver.milian00@gmail.com>
2020-03-21 23:16:13 -0600

    Node Implementation

--- src/translator/Node.java
+++ src/translator/Node.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package translator;
 
+package translator;
+/**
+ * Universidad del Valle de Guatemala
+ * Algoritmos y estructura de datos
+ * 
+ * General Description:
+ * A program which main purpose is to store a wide brand of English words
+ * with their Spanish meaning. Implementing Association, Node and BinaryTree 
+ * classes.
+ * 
+ * Class Description:
+ * Node class, store association and contructors for the class
+ * 
+ * @author olivverde
+ * */
 import java.util.Comparator;
 
 public class Node<E>
 {
+	//Attributes
     private Association<String, String> value;
     protected  Node<E> left;
     protected  Node<E> right;        
@@ -21,13 +31,13 @@ public class Node<E>
         
                 
     }
-
+    //Constructor based on association implementation
     public Node (Association<String, String> value) {
         this.value = value;
         left = null;
         right = null;
     }
-    
+    //Getters 
     public String getKey()
     {
         return value.key;
